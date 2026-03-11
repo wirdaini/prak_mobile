04b94421c78de2ee00e81bd29fb0ef3979a65e73
pertemuan2 <wirda24si@mahasiswa.pcr.ac.id>
2026-03-11 09:35:47 +0700

    first commit

---
+++ app/src/main/java/com/example/indakbanamoapps/pertemuan_2/BelajarKotlin.kt
@@ -0,0 +1,44 @@
+package com.example.indakbanamoapps.pertemuan_2
+
+fun main() {
+    println("Hai rekan-rekan...")
+    println("Selamat datang di bahasa pemrgraman Kotlin")
+
+    println("================")
+
+    var angka = 15
+    println("Hasil dari 125 + 10 = ${angka + 10}")
+
+    val nilaiInt = 10000
+    val nilaiDouble = 100.003
+    val nilaiFloat = 1000.0f
+
+    println("Nilai Integer = $nilaiInt")
+    println("Nilai Double = $nilaiDouble")
+    println("Nilai Float = $nilaiFloat")
+
+    println("=====STRING=====")
+    val huruf = 'a'
+    println("Ini penggunaan karakter '$huruf")
+
+    val nilaiString = "Mawar"
+    println("Halo $nilaiString!\nApa kabar?")
+
+    println("=======KONDISI=======")
+
+    val nilai = 10
+    if(nilai<0)
+        println("Bilangan negatif")
+    else{
+        if(nilai%2 == 0)
+            println("Bilangan Genap")
+        else
+            println("Bilangan Ganjil")
+    }
+
+    println("=========PERULANGAN========")
+    val kampusKu: Array<String> = arrayOf("Kampus", "Politeknik", "Caltex", "Riau")
+    for (kampus: String in kampusKu) {
+        println(kampus)
+    }
+}
